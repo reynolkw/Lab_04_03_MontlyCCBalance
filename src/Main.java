5eb862f36184ff9dcd8c1103e63bc922c8dfc0c0
hueforge <111766391+reynolkw@users.noreply.github.com>
2023-09-16 23:37:58 -0400

    added functionality

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // simulating user input
+        double ccBalance = 5000.0;
+        double interestRate = 0.17;
+        double interestDue = 0.0;
+
+        interestDue = ccBalance * interestRate;
+        ccBalance = ccBalance + interestDue;
+        System.out.println("The interest due after one month is $" + interestDue);
+        System.out.println("The account balance after one month is $" + ccBalance);
+
+        System.out.println();
+
+        interestDue = ccBalance * interestRate;
+        ccBalance = ccBalance + interestDue;
+        System.out.println("The interest due after two months is $" + interestDue);
+        System.out.println("The account balance after two months is $" + ccBalance);
+
     }
 }
